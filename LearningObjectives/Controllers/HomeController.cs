6f2dca094fb81ddfbed4d19c28cb63eb8c4a6fb9
Alex Beutler <JABeutler2195@gmail.com>
2019-08-31 15:57:44 -0800

    Add all items to repo.

---
+++ LearningObjectives/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LearningObjectives.Models;
+
+namespace LearningObjectives.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //  This is presumably how the HomeController is supposed to be modified to support the Course view.
+        public IActionResult Course()
+        {
+            return View();
+        }
+    }
+}
